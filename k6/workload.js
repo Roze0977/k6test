a7c085459e29426694a84907d0a1f02aa266d559
graphite <119034361+Roze0977@users.noreply.github.com>
2024-02-11 00:57:49 +0900

    Update workload.js

--- k6/workload.js
+++ k6/workload.js
@@ -2,10 +2,10 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  vus: 1,
-  iterations: 1000000000
+  vus: 270000,
+  iterations: 10000000
 };
 export default function () {
   http.get('https://seized.ws/nosec');
-  sleep(0.000001);
+  sleep(0.001);
 }
